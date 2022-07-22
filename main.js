58ce7f8b102f6591b2ef4d9f563634426b3c2716
gregkillebrew <gkillebrew91@gmail.com>
2022-07-22 12:10:46 -0500

    started

---
+++ main.js
@@ -0,0 +1,41 @@
+// Whiteboard, make a code plan and build each of the following methods:
+
+// .map() - function that takes an array of items and a function that returns an array with each item manipulated by that function.
+const numbers = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction)
+
+
+// .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
+let motorcycles = [{motorcycles:"harley", cost: 1000}, {motorcycles:"ducati", cost: 1000}, {motorcycles:"suzuki", cost: 800}, {motorcycles:"kawasaki", cost: 800}, {motorcycles:"indian", cost: 900}]
+
+for (let i = 0; i < motorcycles.length; i++) {
+    sum += numbers[i];
+}
+
+
+motorcycles.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+
+motorcycles.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+
+
+
+reduce()
+
+
+// .filter() - function that takes an array of items and a function that returns an array with only the items that return true in the function.
+filter()
+
+
+// Create a repo called "Higher-Order-From-Scratch".
+
+
+
+// Practice working from branches by creating a new branch called: "HigherOrder".
+
+
+
+// Place your code in a file called main.js.
+
+
+
+// Create a pull request (PR) for Higher-Order-From-Scratch to be merged into master.
